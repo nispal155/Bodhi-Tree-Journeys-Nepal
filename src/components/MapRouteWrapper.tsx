449f6d6cd21fb56e5ecb9177882f53860ae58fc1
nispal155 <nispal.bhattarai15@gmail.com>
2026-07-22 19:57:33 +0545

    chore: remove unused map components and leaflet dependencies

--- src/components/MapRouteWrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const MapRoute = dynamic(() => import("@/components/MapRoute"), { ssr: false });
-
-export default function MapRouteWrapper({ routeString }: { routeString: string }) {
-  return <MapRoute routeString={routeString} />;
-}
